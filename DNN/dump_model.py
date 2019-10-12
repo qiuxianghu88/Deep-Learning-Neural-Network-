1a1a8d32b43e09a84a19d206d582980f0d7785e9
qiuxiang hu <42918681+qiuxianghu88@users.noreply.github.com>
2019-10-12 18:10:40 +0800

    Add files via upload

---
+++ DNN/dump_model.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+import random
+from torch.autograd import Variable
+from os.path import join as join_path
+import torch.nn.functional as F
+
+model_path = sys.argv[1]
+checkpoint = torch.load(model_path)
+model_dict = checkpoint['model']
+
+for k, v in model_dict.items():
+    print k
+    model_sub_file_name = k
+    model_sub_file_path = join_path('./dump', model_sub_file_name)
+    f = open(model_sub_file_path, 'w')
+    value = model_dict[k].cpu().numpy()
+    value.dump(f)
+    f.close()
